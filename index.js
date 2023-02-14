c6c21adbbbc5feeb38962a319f2170713970b111
prof0x <125227012+prof0x@users.noreply.github.com>
2023-02-14 13:38:46 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const superbowl = require("./superbowl-lvii-events.json")
+
+const run = async () => {
+    console.log("Hello Bowl!\n")
+
+    console.log("Properties available in our json data:\n")
+    console.log(Object.keys(superbowl[0]))
+
+    superbowl.forEach(event => {
+        console.log('\nquarter: ', event.quarter,  
+                    '\ntime left: ', event.minute, 
+                    '\nteam: ', event.team.name,     
+                    '\nplayer: ', event.player.name, 
+                    '\ntype: ', event.type,     
+                    '\ncomment: ', event.comment, 
+                    '\nscore: ', event.score.away, ' - ', event.score.home)
+    })
+}
+
+run();
